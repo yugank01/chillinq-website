009fb8945d4b1b4e4431342692eb10119f6f7f45
unknown <yugankbansal.00001276033.aesl@gmail.com>
2023-10-04 14:37:14 +0530

    responsiveness

--- app/ components/Heading.jsx
+++ app/ components/Heading.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Heading = () => {
   return (
     <div className="wrapper flex flex-col p-4">
-      <div className="content text-[5.5rem] sm:text-[10rem] ">
+      <div className="content text-[4rem] sm:text-[10rem] ">
         FEATURES
       </div>
-      <div className='absolute text-white font-semibold text-center text-[3rem] sm:text-[5rem]'>
+      <div className='absolute text-white font-semibold text-center text-[2.5rem] sm:text-[5rem]'>
         FEATURES
       </div>
     </div>
