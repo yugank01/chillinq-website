757ba6d16dc6dc2be64694bdcb78efad4551938f
unknown <yugankbansal.00001276033.aesl@gmail.com>
2023-10-10 18:31:18 +0530

    tweets fixed

--- app/ components/Twitter.jsx
+++ app/ components/Twitter.jsx
@@ -4,36 +4,111 @@ import { Tweet } from 'react-tweet'
 const Twitter = () => {
   return (
     <div>
-                <div>
-          <h1 className="feature-h1">Hear from our Community</h1>
+      <div className="App px-4 mx-4 sm:mx-12 grid grid-cols-1" data-theme="light" >
+        {/* <Tweet id="1659273134879514624" /> */}
+        {/* <Tweet id="1679751466477838342" />
+        <Tweet id="1666349318176129025" />
+        <Tweet id="1655974876136738818" />
+        <Tweet id="1655305248934891520" />
+        <Tweet id="1654803389149233152" />
+        <Tweet id="1649015193517821955" /> */}
+        {/* <Tweet id="1680924487032659969" /> */}
 
+        <div>
+            <div className="use-slider">
+              <div className="use-slide">
+
+                <div className=" ">
+                  <Tweet id="1679751466477838342" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1666349318176129025" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1655974876136738818" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1655305248934891520" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1654803389149233152" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1649015193517821955" />
+                </div>
+
+
+
+
+                <div className=" ">
+                  <Tweet id="1679751466477838342" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1666349318176129025" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1655974876136738818" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1655305248934891520" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1654803389149233152" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1649015193517821955" />
+                </div>
+
+
+
+
+                <div className=" ">
+                  <Tweet id="1679751466477838342" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1666349318176129025" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1655974876136738818" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1655305248934891520" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1654803389149233152" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1649015193517821955" />
+                </div>
+
+
+
+
+                <div className=" ">
+                  <Tweet id="1679751466477838342" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1666349318176129025" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1655974876136738818" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1655305248934891520" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1654803389149233152" />
+                </div>
+                <div className=" ">
+                  <Tweet id="1649015193517821955" />
+                </div>
+
+              </div>
+            </div>
         </div>
-        
-            <div className="App px-12 mx-12 sm:grid-cols-1 grid lg:grid-cols-3 gap-8" data-theme="light" >
-     
-     {/* <Tweet id="1659273134879514624" /> */}
-     <Tweet id="1679751466477838342" />
-     <Tweet id="1666349318176129025" />
-     <Tweet id="1655974876136738818" />
-     <Tweet id="1655305248934891520" />
-     <Tweet id="1654803389149233152" />
-     <Tweet id="1649015193517821955" />
 
-
-
-
-
-
-
-
-    
-
-     {/* <Tweet id="1680924487032659969" /> */}
-
-   </div>
+      </div>
     </div>
-
-
   )
 }
 
