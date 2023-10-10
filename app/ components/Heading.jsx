367708a77b073b13307676bdf4b0b97e07beca71
unknown <yugankbansal.00001276033.aesl@gmail.com>
2023-10-11 00:33:04 +0530

    heading opacity optimized

--- app/ components/Heading.jsx
+++ app/ components/Heading.jsx
@@ -11,7 +11,7 @@ const Heading = () => {
     //   </div>
     // </div>
     <div className='h-[40vh] sm:h-[80vh] flex flex-col relative justify-center items-center w-full'>
-      <h1 className='relative opacity-40 features bg-clip-text text-transparent text-center bg-gradient-to-r from-[#52B450] via-[#E4D75A] to-[#31B4BC] via-[#365DC1] to-[#D794EF] to-[#B22E2E] text-[2.5rem] md:text-[6.5rem] lg:text-[9rem]'>FEATURES</h1>  
+      <h1 className='relative opacity-60 features bg-clip-text text-transparent text-center bg-gradient-to-r from-[#52B450] via-[#E4D75A] to-[#31B4BC] via-[#365DC1] to-[#D794EF] to-[#B22E2E] text-[2.5rem] md:text-[6.5rem] lg:text-[9rem]'>FEATURES</h1>  
       <div className='absolute text-white tracking-widest font-semibold text-center text-[2rem] md:text-[5rem]'>     FEATURES
      </div>
     </div>
